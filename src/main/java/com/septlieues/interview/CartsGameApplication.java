2c4d76dfaa96fc3ec8ff2296b57e152cda664a0a
msekkoume <mohamed.sekkoume@gmail.com>
2023-01-18 22:28:55 +0100

    Initial commit

---
+++ src/main/java/com/septlieues/interview/CartsGameApplication.java
@@ -0,0 +1,13 @@
+package com.septlieues.interview;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CartsGameApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CartsGameApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/septlieues/interview/CartsGameApplicationTests.java
@@ -0,0 +1,13 @@
+package com.septlieues.interview;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CartsGameApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
